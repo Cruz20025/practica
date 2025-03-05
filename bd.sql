cf7d378c8562f4ea79aa84f78b194cb85cfd5c03
Cruz20025 <202014024@uns.edu.pe>
2025-03-05 09:50:37 -0500

    Create bd.sql

---
+++ bd.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Personal;
+USE Personal;
+CREATE TABLE Alumno (
+  id INT PRIMARY KEY,
+  nombres VARCHAR(100),
+  apellidos VARCHAR(100),
+  edad VARCHAR(100),
+  telefono VARCHAR(100),
+  correo VARCHAR(100) 
+  );
+INSERT INTO (id,nombres,apellidos,edad,telefono,correo) VALUES (1,'Ruben Gonzalo','Cruz Acosta','24 años','996701532','acostakaku420@gmail.com');
